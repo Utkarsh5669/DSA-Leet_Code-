081570f655577c3e79e5b5234dc7001df5d48efb
Utkarsh5669 <116023139+Utkarsh5669@users.noreply.github.com>
2024-03-19 23:45:37 +0530

    Time: 3 ms (83.79%) | Memory: 46.1 MB (14.57%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,30 +1,17 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        Map<Character, Integer> taskFrequency = new HashMap<>();
+       int[] freq = new int[26];
         for (char task : tasks) {
-            taskFrequency.put(task, taskFrequency.getOrDefault(task, 0) + 1);
+            freq[task - 'A']++;
+        }
+        Arrays.sort(freq);
+        int chunk = freq[25] - 1;
+        int idle = chunk * n;
+
+        for (int i = 24; i >= 0; i--) {
+            idle -= Math.min(chunk, freq[i]);
         }
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        maxHeap.addAll(taskFrequency.values());
-        int intervals = 0;
-
-        while (!maxHeap.isEmpty()) {
-            List<Integer> temp = new ArrayList<>();
-            for (int i = 0; i < n + 1; i++) {
-                if (!maxHeap.isEmpty()) {
-                    temp.add(maxHeap.remove());
-                }
-            }
-
-            for (int freq : temp) {
-                if (--freq > 0) {
-                    maxHeap.add(freq);
-                }
-            }
-            intervals += maxHeap.isEmpty() ? temp.size() : n + 1;
-        }
-
-        return intervals;
+        return idle < 0 ? tasks.length : tasks.length + idle;
     }
 }
