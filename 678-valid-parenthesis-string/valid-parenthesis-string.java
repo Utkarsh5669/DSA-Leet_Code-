ec545e8a09c9ba2db5859037fa42c420b4b9a9c2
Utkarsh5669 <116023139+Utkarsh5669@users.noreply.github.com>
2024-04-08 00:09:38 +0530

    Time: 1 ms (38.41%) | Memory: 41.3 MB (42.85%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Integer> leftStack = new Stack<>();
+        Stack<Integer> starStack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                leftStack.push(i);
+            } else if (c == '*') {
+                starStack.push(i);
+            } else { // c == ')'
+                if (!leftStack.isEmpty()) {
+                    leftStack.pop();
+                } else if (!starStack.isEmpty()) {
+                    starStack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        while (!leftStack.isEmpty() && !starStack.isEmpty()) {
+            if (leftStack.pop() > starStack.pop()) {
+                return false;
+            }
+        }
+
+        return leftStack.isEmpty();
+    }
+}
