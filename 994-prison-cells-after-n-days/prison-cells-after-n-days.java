266facdd397f2ec9292a6a13ab1d2affdf49d13e
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-08-11 01:04:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (97.36%) - LeetSync

--- 994-prison-cells-after-n-days/prison-cells-after-n-days.java
+++ 994-prison-cells-after-n-days/prison-cells-after-n-days.java
@@ -1,41 +1,55 @@
 
 
-class Solution {
-    public int[] prisonAfterNDays(int[] cells, int N) {
-        Map<String, Integer> seen = new HashMap<>();
-        boolean hasCycle = false;
-        int cycleLength = 0;
+// class Solution {
+//     public int[] prisonAfterNDays(int[] cells, int N) {
+//         Map<String, Integer> seen = new HashMap<>();
+//         boolean hasCycle = false;
+//         int cycleLength = 0;
         
-        for (int i = 0; i < N; i++) {
-            int[] next = nextDay(cells);
-            String stateKey = Arrays.toString(next);
+//         for (int i = 0; i < N; i++) {
+//             int[] next = nextDay(cells);
+//             String stateKey = Arrays.toString(next);
             
-            if (seen.containsKey(stateKey)) {
-                hasCycle = true;
-                cycleLength = i - seen.get(stateKey);
-                break;
-            } else {
-                seen.put(stateKey, i);
-            }
+//             if (seen.containsKey(stateKey)) {
+//                 hasCycle = true;
+//                 cycleLength = i - seen.get(stateKey);
+//                 break;
+//             } else {
+//                 seen.put(stateKey, i);
+//             }
             
-            cells = next;
-        }
+//             cells = next;
+//         }
         
-        if (hasCycle) {
-            N %= cycleLength;
-            for (int i = 0; i < N; i++) {
-                cells = nextDay(cells);
-            }
-        }
+//         if (hasCycle) {
+//             N %= cycleLength;
+//             for (int i = 0; i < N; i++) {
+//                 cells = nextDay(cells);
+//             }
+//         }
         
-        return cells;
-    }
+//         return cells;
+//     }
     
-    private int[] nextDay(int[] cells) {
+//     private int[] nextDay(int[] cells) {
+//         int[] newCells = new int[cells.length];
+//         for (int i = 1; i < cells.length - 1; i++) {
+//             newCells[i] = cells[i - 1] == cells[i + 1] ? 1 : 0;
+//         }
+//         return newCells;
+//     }
+// }
+class Solution {
+    public int[] prisonAfterNDays(int[] cells, int n) {
+        n=(n-1)%14+1;
         int[] newCells = new int[cells.length];
-        for (int i = 1; i < cells.length - 1; i++) {
-            newCells[i] = cells[i - 1] == cells[i + 1] ? 1 : 0;
-        }
-        return newCells;
+        if (n==0)
+            return cells;
+        for (int i=1;i<cells.length-1;i++)
+            if (cells[i-1]==cells[i+1])
+                newCells[i]=1;
+            else
+                newCells[i]=0;
+        return prisonAfterNDays(newCells,n-1);
     }
 }
