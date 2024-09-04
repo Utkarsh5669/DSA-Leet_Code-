4db2195a51edad33eec2d7e790cd406fb950d6be
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-09-04 23:39:50 +0530

    Time: 35 ms (48.91%) | Memory: 48.3 MB (95.63%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.java
+++ 906-walking-robot-simulation/walking-robot-simulation.java
@@ -1,33 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int robotSim(int[] commands, int[][] obstacles) {
-        Set<String> obstacleSet = new HashSet<>();
-        for (int[] obstacle : obstacles) {
-            obstacleSet.add(obstacle[0] + "," + obstacle[1]);
+        int x=0,y=0,d=0;
+        int[][] direction={{0,1},{1,0},{0,-1},{-1,0}};
+        int maxDistance=0;
+        Set<String> obstacleSet=new HashSet<>();
+        for(int[] obstacle:obstacles){
+            obstacleSet.add(obstacle[0]+","+obstacle[1]);
         }
-
-        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // North, East, South, West
-        int x = 0, y = 0, direction = 0, maxDistance = 0;
-
-        for (int command : commands) {
-            if (command == -1) {
-                direction = (direction + 1) % 4;
-            } else if (command == -2) {
-                direction = (direction + 3) % 4;
-            } else {
-                for (int i = 0; i < command; i++) {
-                    int newX = x + directions[direction][0];
-                    int newY = y + directions[direction][1];
-                    if (!obstacleSet.contains(newX + "," + newY)) {
-                        x = newX;
-                        y = newY;
-                        maxDistance = Math.max(maxDistance, x * x + y * y);
-                    } else {
+        for(int cmd:commands){
+            if(cmd==-1){
+                d=(d+1)%4;
+            }else if(cmd==-2){
+                d=(d+3)%4;
+            }else{
+                for(int i=0;i<cmd;i++){
+                    int nx=x+direction[d][0];
+                    int ny=y+direction[d][1];
+                    if(obstacleSet.contains(nx+","+ny)){
                         break;
                     }
+                    x=nx;
+                    y=ny;
+                    maxDistance=Math.max(maxDistance,x*x+y*y);
                 }
             }
         }
-
         return maxDistance;
     }
 }
