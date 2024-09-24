3ef2bc7cd003c2ac025435c120fae95486374055
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-09-24 14:42:27 +0530

    Time: 88 ms (49.29%) | Memory: 55.4 MB (74.55%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
@@ -1,31 +1,39 @@
-import java.util.HashMap;
-
 class Solution {
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
-        HashMap<String, Integer> prefixMap = new HashMap<>();
+        HashMap<Integer, Integer> prefix = new HashMap<>();
         
-        for (int num : arr1) {
-            String strNum = Integer.toString(num);
-            String prefix = "";
-            for (char ch : strNum.toCharArray()) {
-                prefix += ch;
-                prefixMap.put(prefix, prefixMap.getOrDefault(prefix, 0) + 1);
+        // Store all prefixes of the first array
+        for (int val : arr1) {
+            int num = val;
+            while (num > 0) {
+                prefix.put(num, prefix.getOrDefault(num, 0) + 1);
+                num /= 10;
             }
         }
-        
-        int maxLength = 0;
-        
-        for (int num : arr2) {
-            String strNum = Integer.toString(num);
-            String prefix = "";
-            for (char ch : strNum.toCharArray()) {
-                prefix += ch;
-                if (prefixMap.containsKey(prefix)) {
-                    maxLength = Math.max(maxLength, prefix.length());
+
+        // Variable to track the maximum length
+        int mx = Integer.MIN_VALUE;
+
+        // Instead of creating another map, check the prefix at the moment
+        for (int val : arr2) {
+            int num = val;
+            // Count the number of digits in num
+            int len = (int) Math.log10(num) + 1;
+
+            // Generate all prefixes again
+            while (num > 0) {
+                // If prefix found, break
+                if (prefix.containsKey(num)) {
+                    break;
                 }
+                num /= 10;
+                // Decrease digit count as we decrease prefix
+                len--;
             }
+
+            mx = Math.max(mx, len);
         }
-        
-        return maxLength;
+
+        return mx;
     }
 }
