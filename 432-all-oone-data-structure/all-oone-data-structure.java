b3b2cbc06056f7f6d9c5ed85fb77ac15180865b7
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-09-29 18:39:58 +0530

    Time: 52 ms (71.32%) | Memory: 60.9 MB (41.36%) - LeetSync

--- 432-all-oone-data-structure/all-oone-data-structure.java
+++ 432-all-oone-data-structure/all-oone-data-structure.java
@@ -1,34 +1,120 @@
-class AllOne {
-    private Map<String, Integer> count;
-    private TreeSet<Pair<Integer, String>> set;
+public class Node {
 
-    public AllOne() {
-        count = new HashMap<>();
-        set = new TreeSet<>((a, b) -> a.getKey().equals(b.getKey()) ? a.getValue().compareTo(b.getValue()) : a.getKey() - b.getKey());
+    int freq;
+    Node prev;
+    Node next;
+    Set<String> strSet = new HashSet<>();
+
+    Node(int freq) {
+        this.freq = freq;
+    }
+}
+
+class AllOne {
+
+    Node head;
+    Node tail;
+    Map<String, Node> strToNodeMap = new HashMap<>();
+
+    AllOne() {
+        head = new Node(0);
+        tail = new Node(0);
+        head.next = tail;
+        tail.prev = head;
     }
 
     public void inc(String key) {
-        int n = count.getOrDefault(key, 0);
-        count.put(key, n + 1);
-        set.remove(new Pair<>(n, key));
-        set.add(new Pair<>(n + 1, key));
+        if (strToNodeMap.containsKey(key)) {
+            Node node = strToNodeMap.get(key);
+            int freq = node.freq;
+            node.strSet.remove(key);
+
+            Node nextNode = node.next;
+            if (nextNode == tail || nextNode.freq != freq + 1) {
+                Node newNode = new Node(freq + 1);
+                newNode.strSet.add(key);
+                newNode.prev = node;
+                newNode.next = nextNode;
+                node.next = newNode;
+                nextNode.prev = newNode;
+                strToNodeMap.put(key, newNode);
+            } else {
+                nextNode.strSet.add(key);
+                strToNodeMap.put(key, nextNode);
+            }
+
+            if (node.strSet.isEmpty()) {
+            deleteNode(node);
+            }
+        }
+         else {
+            Node firstNode = head.next;
+            if (firstNode == tail || firstNode.freq > 1) {
+                Node newNode = new Node(1);
+                newNode.strSet.add(key);
+                newNode.prev = head;
+                newNode.next = firstNode;
+                head.next = newNode;
+                firstNode.prev = newNode;
+                strToNodeMap.put(key, newNode);
+            } else {
+                firstNode.strSet.add(key);
+                strToNodeMap.put(key, firstNode);
+            }
+        }
     }
 
     public void dec(String key) {
-        int n = count.get(key);
-        set.remove(new Pair<>(n, key));
-        if (n == 1) count.remove(key);
-        else {
-            count.put(key, n - 1);
-            set.add(new Pair<>(n - 1, key));
+        if (!strToNodeMap.containsKey(key)) {
+            return;
+        }
+
+        Node node = strToNodeMap.get(key);
+        node.strSet.remove(key);
+        int freq = node.freq;
+
+        if (freq == 1) {
+            strToNodeMap.remove(key);
+        } else {
+            Node prevNode = node.prev;
+            if (prevNode == head || prevNode.freq != freq - 1) {
+                Node newNode = new Node(freq - 1);
+                newNode.strSet.add(key);
+                newNode.prev = prevNode;
+                newNode.next = node;
+                prevNode.next = newNode;
+                node.prev = newNode;
+                strToNodeMap.put(key, newNode);
+            } else {
+                prevNode.strSet.add(key);
+                strToNodeMap.put(key, prevNode);
+            }
+        }
+
+        if (node.strSet.isEmpty()) {
+            deleteNode(node);
         }
     }
 
     public String getMaxKey() {
-        return set.isEmpty() ? "" : set.last().getValue();
+        if (tail.prev == head) {
+            return "";
+        }
+        return tail.prev.strSet.iterator().next();
     }
 
     public String getMinKey() {
-        return set.isEmpty() ? "" : set.first().getValue();
+        if (head.next == tail) {
+            return "";
+        }
+        return head.next.strSet.iterator().next();
+    }
+
+    private void deleteNode(Node node) {
+        Node prevNode = node.prev;
+        Node afterNode = node.next;
+
+        prevNode.next = afterNode;
+        afterNode.prev = prevNode;
     }
 }
