ebc3c498fae1e6de1e896122debc1fc05fff7bb6
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-10-03 00:29:39 +0530

    Time: 5 ms (100.00%) | Memory: 60.1 MB (81.90%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,28 +1,28 @@
-class ValueIndex
-{
-  int val;
-  int index;
-  public ValueIndex(int val, int index)
-  {
-    this.val = val;
-    this.index = index;
-  }  
-}
-
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-    PriorityQueue<ValueIndex> pq = new PriorityQueue<>((a, b)->a.val-b.val);    
-    for(int i=0; i<arr.length; i++)
-    pq.add(new ValueIndex(arr[i], i));
-    int rank=0, prev = Integer.MIN_VALUE;
-    while(!pq.isEmpty())
-    {
-      ValueIndex temp = pq.poll();
-      if(temp.val>prev)
-      rank++;
-      arr[temp.index] = rank;
-      prev = temp.val;
+        if (arr == null || arr.length == 0) return new int[0];
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int x : arr) {
+            min = Math.min(min, x);
+            max = Math.max(max, x);
+        }
+        int range = max - min + 1;
+        if (range > 2_000_000) return sortMethod(arr);
+        boolean[] count = new boolean[range];
+        for (int x : arr) count[x - min] = true;
+        int[] ranks = new int[range];
+        for (int i = 0, r = 1; i < range; i++) if (count[i]) ranks[i] = r++;
+        for (int i = 0; i < arr.length; i++) arr[i] = ranks[arr[i] - min];
+        return arr;
     }
-    return arr;
+    private int[] sortMethod(int[] arr) {
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+        Map<Integer, Integer> rankMap = new HashMap<>();
+        for (int i = 0, r = 1; i < sorted.length; i++) {
+            if (!rankMap.containsKey(sorted[i])) rankMap.put(sorted[i], r++);
+        }
+        for (int i = 0; i < arr.length; i++) arr[i] = rankMap.get(arr[i]);
+        return arr;
     }
 }
