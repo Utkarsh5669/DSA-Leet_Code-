46338f89835179ae08acd6677184ac85e257077f
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-10-05 17:52:24 +0530

    Time: 6 ms (86.36%) | Memory: 42.1 MB (98.60%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,50 +1,49 @@
 class Solution {
+
+    int[] count1 = new int[26];
+    int[] count2 = new int[26];
+
+    public boolean equalPermutation() {
+        for (int i = 0; i < 26; i++) {
+            if (count1[i] != count2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public boolean checkInclusion(String s1, String s2) {
-    int n1 = s1.length();
-    int n2 = s2.length();
-   
-    if(n1>n2)
-    return false;
-    if(n1==0)
-    return true;
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+        int n1 = s1.length();
+        int n2 = s2.length();
 
-    int a[] = new int[26];
-    int b[] = new int[26];
+        // initializing zero
+        for (int i = 0; i < 26; i++) {
+            count1[i] = 0;
+            count2[i] = 0;
+        }
 
+        // apply count on same size of n1
+        for (int i = 0; i < n1; i++) {
+            count1[s1.charAt(i) - 'a']++;
+            count2[s2.charAt(i) - 'a']++;
+        }
 
-    for(int i=0; i<n1; i++)
-    {
-     a[s1.charAt(i)-'a']++;
-     b[s2.charAt(i)-'a']++;   
+        if (equalPermutation()) {
+            return true;
+        }
+
+        // sliding window
+        for (int i = n1; i < n2; i++) {
+            count2[s2.charAt(i) - 'a']++;
+            count2[s2.charAt(i - n1) - 'a']--;
+            if (equalPermutation()) {
+                return true;
+            }
+        }
+
+        return false;
     }
-    
-    int freq = 0;
-    for(int i=0; i<26; i++)
-    if(a[i]==b[i])
-    freq++;
-
-    for(int i=0; i<n2-n1; i++)
-    {
-      if(freq==26)
-      return true;
-
-      int left = s2.charAt(i)-'a';
-      int right = s2.charAt(i+n1)-'a';
-      b[right]++;
-      if(a[right]==b[right])
-      freq++;
-      else if(a[right]+1==b[right])
-      freq--;
-
-      b[left]--;
-      if(a[left]==b[left])
-      freq++;
-      else if(a[left]-1==b[left])
-      freq--;
-
-    }
-      return freq==26;
-    
-    }
-
 }
