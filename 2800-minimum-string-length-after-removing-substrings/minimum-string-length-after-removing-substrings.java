cdd7823343040465509b960102d93c273c99966f
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-10-07 13:04:06 +0530

    Time: 4 ms (85.00%) | Memory: 43.4 MB (42.63%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -1,26 +1,24 @@
-class Solution {
+import java.util.Stack;
+
+public class Solution {
     public int minLength(String s) {
         Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char cur_char = s.charAt(i);
-
-            if (stack.isEmpty()) {
-                stack.push(cur_char);
-                continue;
-            }
-      
-            if (cur_char == 'B' && stack.peek() == 'A') {
+        
+        for (char x : s.toCharArray()) {
+            if (x == 'B' && !stack.isEmpty() && stack.peek() == 'A') {
                 stack.pop();
-            }
-            else if (cur_char == 'D' && stack.peek() == 'C') {
+            } else if (x == 'D' && !stack.isEmpty() && stack.peek() == 'C') {
                 stack.pop();
-            }
-            else {
-                stack.push(cur_char);
+            } else {
+                stack.push(x);
             }
         }
-
+        
         return stack.size();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.minLength("ABCD")); // Example usage
     }
 }
