105c2fae44f72a8608b7e92deef23d1e92d3c8e1
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-10-15 15:47:37 +0530

    Time: 7 ms (100.00%) | Memory: 45.6 MB (20.43%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
@@ -1,15 +1,16 @@
 class Solution {
     public long minimumSteps(String s) {
-        long ans = 0;
-        long zeroCnt = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            char ch = s.charAt(i); 
-            if (ch == '0') {
-                zeroCnt++;
-            } else if (ch == '1') {
-                ans += zeroCnt;
+        long count = 0;
+        long zeroNum = 0;
+        for(int i = s.length() - 1; i >= 0; i--){
+            char c = s.charAt(i);
+            if(c == '0'){
+                zeroNum++;
+            } else{
+                count += zeroNum;
             }
+
         }
-        return ans;
+        return count;
     }
 }
