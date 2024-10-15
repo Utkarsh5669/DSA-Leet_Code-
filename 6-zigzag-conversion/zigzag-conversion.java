979a180193b6a98689d2f9dd38d50b6cd450a5af
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-10-15 16:05:03 +0530

    Time: 2 ms (99.55%) | Memory: 44.9 MB (63.77%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,32 +1,32 @@
 class Solution {
     public String convert(String s, int numRows) {
-       if (numRows == 1 || numRows >= s.length()) {
+        if (numRows == 1) {
             return s;
         }
-
-        int idx = 0, d = 1;
-        List<Character>[] rows = new ArrayList[numRows];
+        
+        StringBuilder answer = new StringBuilder();
+        int n = s.length();
+        int diff = 2 * (numRows - 1);
+        int diagonalDiff = diff;
+        int secondIndex;
+        int index;
         for (int i = 0; i < numRows; i++) {
-            rows[i] = new ArrayList<>();
-        }
+            index = i;
 
-        for (char c : s.toCharArray()) {
-            rows[idx].add(c);
-            if (idx == 0) {
-                d = 1;
-            } else if (idx == numRows - 1) {
-                d = -1;
-            }
-            idx += d;
-        }
-
-        StringBuilder result = new StringBuilder();
-        for (List<Character> row : rows) {
-            for (char c : row) {
-                result.append(c);
+            while (index < n) {
+                answer.append(s.charAt(index));
+                if (i != 0 && i != numRows - 1) {
+                    diagonalDiff = diff-2*i;
+                    secondIndex = index + diagonalDiff;
+                    
+                    if (secondIndex < n) {
+                        answer.append(s.charAt(secondIndex));
+                    }
+                }
+                index += diff;
             }
         }
-
-        return result.toString();        
+        
+        return answer.toString();
     }
 }
