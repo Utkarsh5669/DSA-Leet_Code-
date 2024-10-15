082cd00cf74325a5f2c01207942c83eb2c6f15be
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-10-15 16:03:04 +0530

    Time: 13 ms (95.68%) | Memory: 42.4 MB (70.14%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,29 +1,25 @@
 public class Solution {
-    public String longestPalindrome(String s) {
-        if (s == null || s.length() < 1) return ""; // edge case
-        
-        int start = 0, end = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);   // Check for odd length palindrome
-            int len2 = expandAroundCenter(s, i, i + 1);   // Check for even length palindrome
-            int len = Math.max(len1, len2);   // Get the max length
-            
-            if (len > end - start) {    // If we found a longer palindrome
-                start = i - (len - 1) / 2;    // Update the start index
-                end = i + len / 2;    // Update the end index
-            }
-        }
-        
-        return s.substring(start, end + 1);  // Return the longest palindrome substring
+private int lo, maxLen;
+
+public String longestPalindrome(String s) {
+	int len = s.length();
+	if (len < 2)
+		return s;
+	
+    for (int i = 0; i < len-1; i++) {
+     	extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
+     	extendPalindrome(s, i, i+1); //assume even length.
     }
-    
-    // Expands around the center and returns the length of the palindrome
-    private int expandAroundCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-        return right - left - 1;  // Return the length of the palindrome
-    }
+    return s.substring(lo, lo + maxLen);
 }
+
+private void extendPalindrome(String s, int j, int k) {
+	while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+		j--;
+		k++;
+	}
+	if (maxLen < k - j - 1) {
+		lo = j + 1;
+		maxLen = k - j - 1;
+	}
+}}
