d7efeb2c2ee084efd4a8732c8bca00a665d0be08
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-10-22 13:39:03 +0530

    Time: 30 ms (75.42%) | Memory: 61.4 MB (95.20%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.java
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.java
@@ -1,47 +1,42 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public long kthLargestLevelSum(TreeNode root, int k) {
-        List<Long> list = new ArrayList<>(); // List to Store the Long_Level_Sum
+    PriorityQueue<Long> pq = new PriorityQueue<>();
+    Queue<TreeNode> q = new LinkedList<>();
+    q.add(root);
 
-        // BFS Traversal......
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            long sum = 0;
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                sum += node.val;
-                if (node.left != null) {
-                    queue.offer(node.left);
-                }
-                if (node.right != null) {
-                    queue.offer(node.right);
-                }
-            }
-            list.add(sum);
+    while(!q.isEmpty())
+    {
+        int size = q.size();
+        long sum = 0;
+        while(size>0)
+        {
+           TreeNode temp = q.poll();
+           sum += temp.val;
+           if(temp.left!=null)
+           q.add(temp.left);
+           if(temp.right!=null)
+           q.add(temp.right);
+           size--;
         }
-
-        if (list.size() < k) {
-            return -1; // Edge or Cornor Case
-        }
-
-        Collections.sort(list);
-        return list.get(list.size() - k);
+        pq.add(sum);
+        if(pq.size()>k)
+        pq.poll();
+    }    
+      return pq.size()<k ? -1 : pq.peek();    
     }
 }
