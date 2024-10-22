095e952ed758931be1873bf0653c9d0f9ffe44ea
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-10-22 13:38:14 +0530

    Time: 36 ms (47.03%) | Memory: 62.5 MB (74.15%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.java
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.java
@@ -1,42 +1,47 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public long kthLargestLevelSum(TreeNode root, int k) {
-        List<Long> res = new ArrayList<>();  // To store sum of each level
-        Queue<TreeNode> q = new LinkedList<>();  // Queue for level-order traversal
-        q.add(root);  // Start BFS from the root node
+        List<Long> list = new ArrayList<>(); // List to Store the Long_Level_Sum
 
-        while (!q.isEmpty()) {
-            int n = q.size();  // Number of nodes at the current level
-            long sum = 0;  // Sum of node values at the current level
-            
-            for (int i = 0; i < n; i++) {
-                TreeNode node = q.poll();
+        // BFS Traversal......
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            long sum = 0;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
                 sum += node.val;
-                
-                if (node.left != null) q.add(node.left);
-                if (node.right != null) q.add(node.right);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
             }
-            res.add(sum);  // Store the sum of the current level
+            list.add(sum);
         }
 
-        if (k > res.size()) return -1;
-        
-        res.sort(Collections.reverseOrder());  // Sort level sums in descending order
-        
-        return res.get(k - 1);  // Return the k-th largest sum
+        if (list.size() < k) {
+            return -1; // Edge or Cornor Case
+        }
+
+        Collections.sort(list);
+        return list.get(list.size() - k);
     }
 }
