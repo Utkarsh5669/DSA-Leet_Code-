d2d9484e03d0ed47d209721a389c01a207d5b542
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-11-03 22:58:44 +0530

    Time: 1 ms (84.66%) | Memory: 42.7 MB (89.69%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,20 +1,48 @@
+// class Solution {
+//     public List<String> generateParenthesis(int n) {
+//         List<String> res = new ArrayList<String>();
+//         recurse(res, 0, 0, "", n);
+//         return res;
+//     }
+
+//     public void recurse(List<String> res, int left, int right, String s, int n){
+//         if (s.length() == n*2){
+//             res.add(s);
+//             return;
+//         }
+//         if(left<n){
+//             recurse(res, left+1, right, s + "(", n);
+//         }
+//         if(right<left){
+//             recurse(res, left, right+1, s + ")", n);
+//         }
+//     }
+// }
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> res = new ArrayList<String>();
-        recurse(res, 0, 0, "", n);
-        return res;
+        List<String> answer = new ArrayList<>();
+        backtracking(answer, new StringBuilder(), 0, 0, n);
+
+        return answer;
     }
 
-    public void recurse(List<String> res, int left, int right, String s, int n){
-        if (s.length() == n*2){
-            res.add(s);
+    private void backtracking (List<String> answer, StringBuilder curString, int leftCount, int rightCount, int n) {
+        int l = curString.length();
+
+        if (l == 2 *n) {
+            answer.add(curString.toString());
             return;
         }
-        if(left<n){
-            recurse(res, left+1, right, s + "(", n);
+
+        if (leftCount< n){
+            curString.append("(");
+            backtracking(answer, curString, leftCount+1, rightCount, n);
+            curString.deleteCharAt(curString.length() -1);
         }
-        if(right<left){
-            recurse(res, left, right+1, s + ")", n);
+        if (rightCount< leftCount){
+            curString.append(")");
+            backtracking(answer, curString, leftCount, rightCount+1, n);
+            curString.deleteCharAt(curString.length() -1);
         }
     }
 }
