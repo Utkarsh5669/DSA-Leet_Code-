770dbf68630b24e3841db84ce6af385fde74d48f
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-11-04 22:52:17 +0530

    Time: 13 ms (100.00%) | Memory: 45.8 MB (68.96%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.java
+++ 3451-string-compression-iii/string-compression-iii.java
@@ -1,22 +1,48 @@
+// class Solution {
+//     public String compressedString(String word) {
+//         StringBuilder s = new StringBuilder();
+//         int c=1;
+//         int n = word.length();
+//         int i=1;
+//         while(i<n)
+//         {
+//             if(c==9|| word.charAt(i)!=word.charAt(i-1)){
+//                 s.append(c).append(word.charAt(i-1));
+//                 c=1;
+//                 i++;
+//                 continue;
+//             }
+//             if(word.charAt(i)==word.charAt(i-1)){
+//                 c++;
+//             }
+//             i++;
+//         }
+//         return s.append(c).append(word.charAt(n-1)).toString();
+//     }
+// }
 class Solution {
     public String compressedString(String word) {
-        StringBuilder s = new StringBuilder();
-        int c=1;
-        int n = word.length();
-        int i=1;
-        while(i<n)
-        {
-            if(c==9|| word.charAt(i)!=word.charAt(i-1)){
-                s.append(c).append(word.charAt(i-1));
-                c=1;
-                i++;
-                continue;
+        StringBuilder sb=new StringBuilder();
+        int count=0,j=0;
+        for(int i=0;i<word.length();++i){
+            j=i;
+            char c=word.charAt(i);
+            while(j<word.length() && word.charAt(j)==c){
+                ++count;
+                ++j;
             }
-            if(word.charAt(i)==word.charAt(i-1)){
-                c++;
+            while(count>9){
+                sb.append('9');
+                sb.append(c);
+                count-=9;
+
             }
-            i++;
+            sb.append((char)(count+'0'));
+            sb.append(c);
+            count=0;
+            i=j-1;
+
         }
-        return s.append(c).append(word.charAt(n-1)).toString();
+        return sb.toString();
     }
 }
