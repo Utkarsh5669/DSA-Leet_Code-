62481674759219a4adf1b7620d07137050e5fe94
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-11-29 01:09:57 +0530

    Time: 50 ms (94.61%) | Memory: 66.4 MB (98.81%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.java
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.java
@@ -1,32 +1,47 @@
-class Solution {
-    public int minimumObstacles(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        int[][] distance = new int[m][n];
-        for (int[] row : distance) Arrays.fill(row, Integer.MAX_VALUE);
-        Deque<int[]> dq = new ArrayDeque<>();
+public class Solution {
+    public int[][] dir = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
 
-        distance[0][0] = 0;
-        dq.offerFirst(new int[] {0, 0});
-        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    public int minimumObstacles(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        // Deque for 0-1 BFS
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.add(new int[]{0, 0, 0}); // {row, col, obstacleCount}
+        grid[0][0] = -1; // Mark as visited
 
         while (!dq.isEmpty()) {
-            int[] cell = dq.pollFirst();
-            int x = cell[0], y = cell[1];
-            for (int[] dir : directions) {
-                int nx = x + dir[0], ny = y + dir[1];
-                if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
-                    int newDist = distance[x][y] + grid[nx][ny];
-                    if (newDist < distance[nx][ny]) {
-                        distance[nx][ny] = newDist;
-                        if (grid[nx][ny] == 0) {
-                            dq.offerFirst(new int[] {nx, ny});
-                        } else {
-                            dq.offerLast(new int[] {nx, ny});
-                        }
+            int[] temp = dq.pollFirst();
+            int r = temp[0], c = temp[1], obstacle = temp[2];
+
+            // If we've reached the bottom-right corner, return the obstacle count
+            if (r == n - 1 && c == m - 1) {
+                return obstacle;
+            }
+
+            // Explore all 4 directions
+            for (int[] d : dir) {
+                int row = r + d[0];
+                int col = c + d[1];
+
+                if (isValid(row, col, n, m) && grid[row][col] != -1) {
+                    if (grid[row][col] == 0) {
+                        // Free cell, prioritize by adding to the front
+                        dq.addFirst(new int[]{row, col, obstacle});
+                    } else {
+                        // Obstacle cell, add to the back
+                        dq.addLast(new int[]{row, col, obstacle + 1});
                     }
+                    // Mark as visited
+                    grid[row][col] = -1;
                 }
             }
         }
-        return distance[m-1][n-1];
+
+        return -1; // If no path exists (unlikely in a valid grid)
+    }
+
+    public boolean isValid(int i, int j, int n, int m) {
+        return i >= 0 && j >= 0 && i < n && j < m;
     }
 }
