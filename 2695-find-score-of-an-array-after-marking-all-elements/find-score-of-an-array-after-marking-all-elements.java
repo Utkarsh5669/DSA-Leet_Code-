72b5e5614f7ddb08dd91f0410a9615a1c2d8d474
Utkarsh Arora <116023139+Utkarsh5669@users.noreply.github.com>
2024-12-13 10:33:12 +0530

    Time: 2 ms (100.00%) | Memory: 59 MB (61.11%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
@@ -1,33 +1,15 @@
 class Solution {
     public long findScore(int[] nums) {
-        int n = nums.length;
-        int[][] dp = new int[n][2];
-
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = nums[i];
-            dp[i][1] = i;
-        }
-
-        Arrays.sort(dp, (a, b) -> a[0] - b[0]);
-
-        boolean[] marked = new boolean[n];
-        long result = 0;
-
-        for (int[] arr : dp) {
-            int val = arr[0];
-            int ind = arr[1];
-
-            if (!marked[ind]) {
-                result += val;
-
-                marked[ind] = true;
-                if (ind > 0)
-                    marked[ind - 1] = true;
-                if (ind < n - 1) 
-                    marked[ind + 1] = true;
+        long res = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            int start = i;
+            while (i + 1 < nums.length && nums[i + 1] < nums[i]) {
+                i++;
+            }
+            for (int j = i; j >= start; j -= 2) {
+                res += nums[j];
             }
         }
-
-        return result;
+        return res;
     }
 }
